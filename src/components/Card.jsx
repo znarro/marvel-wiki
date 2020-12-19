9eaf1ed8d451adc975e8ef8ee956c833f5292a4f
Josue Narro <josue.narro.a@gmail.com>
2020-12-19 17:19:46 -0500

    Add getCharacters function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Card = () => {
-  return <article>Card</article>;
+const Card = ({ name, image }) => {
+  return (
+    <article>
+      <p>{name}</p>
+      <img src={image} alt={name} />
+    </article>
+  );
 };
 
 export default Card;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Card from "../../components/Card";
 
 const Home = () => {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function getCharacters() {
+      const result = await axios(
+        "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=fadc172f68445faee5eaabcc6a9d88d2&hash=d05c803f568d39789062fd46f7a70134"
+      );
+      console.log(result.data.data.results);
+      setCharacters(result.data.data.results);
+    }
+
+    getCharacters();
+  }, []);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
+      <h1>Heroes</h1>
+      <div>
+        {characters.map((character) => (
+          <Card
+            key={character.id}
+            name={character.name}
+            image={
+              character.thumbnail.path +
+              "/portrait_small." +
+              character.thumbnail.extension
+            }
+          />
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 };
