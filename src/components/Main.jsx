1a1b039a211c6c169f4948b2e4810b11e6046088
Josue Narro <josue.narro.a@gmail.com>
2020-12-26 19:37:16 -0500

    Add filter functionality

--- src/components/Main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Main = () => {
-  return <main>Main 2</main>;
-};
-
-export default Main;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
-import axios from "axios";
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+import HeroesGrid from "../../components/HeroesGrid";
 import Search from "../../components/Search";
-import Card from "../../components/Card";
 import { HeroContextProvider } from "../../contexts/HeroContext";
 
 const Home = () => {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    async function getCharacters() {
-      const BASE_URL = "https://gateway.marvel.com/v1/public/";
-      const API_KEY = "fadc172f68445faee5eaabcc6a9d88d2";
-      const HASH = "d05c803f568d39789062fd46f7a70134";
-      const LIMIT = 40;
-
-      const result = await axios(
-        `${BASE_URL}characters?ts=1&apikey=${API_KEY}&hash=${HASH}&limit=${LIMIT}`
-      );
-      console.log(result.data.data.results);
-      setCharacters(result.data.data.results);
-    }
-
-    getCharacters();
-  }, []);
-
   return (
     <Box>
       <Heading as="h1" size="xl" align="center">
@@ -32,29 +12,7 @@ const Home = () => {
       </Heading>
       <HeroContextProvider>
         <Search />
-        <SimpleGrid minChildWidth="200px" spacing="40px" p="10">
-          {characters
-            .filter(
-              (item) =>
-                item.thumbnail.path !==
-                "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
-            )
-            .map((character) => {
-              const {
-                id,
-                name,
-                thumbnail: { path, extension },
-              } = character;
-
-              return (
-                <Card
-                  key={id}
-                  name={name}
-                  image={path + "/portrait_incredible." + extension}
-                />
-              );
-            })}
-        </SimpleGrid>
+        <HeroesGrid />
       </HeroContextProvider>
     </Box>
   );
